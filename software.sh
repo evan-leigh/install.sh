b6bcac98f73c1553c4410b9a6cc832f59a7fea54
evan-leigh <51345945+evan-leigh@users.noreply.github.com>
2022-06-20 17:26:32 -0400

    initial commit

---
+++ software.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+echo
+echo "INSTALLING SOFTWARE"
+echo
+
+PKGS=(
+	# Terminal 
+	'kitty'
+	'curl'
+  'lazygit'
+	'ripgrep'
+	'feh'
+  'lsd'
+	'zsh'
+
+	# Functionality 
+	'sxhkd'
+	'nemo'
+
+	# Compsitor 
+  'picom'
+
+	# Notifications 
+  'dunst'
+
+	# Statusline 
+  'polybar'
+
+	# Filepicker 
+  'rofi'
+
+	# Development 
+	'git'
+	'yarn'
+
+	# Browser 
+	'firefox'
+
+	# Media 
+	'scrot'
+	'colorpicker'
+
+	# PDF Viewer
+	'zathura'
+	'zathura-pdf-mupdf'
+
+	# Audio
+	'scrot'
+	'alsa-utils'      # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
+	'alsa-plugins'    # ALSA plugins
+	'pulseaudio'      # Pulse Audio sound components
+	'pulseaudio-alsa' # ALSA configuration for pulse audio
+)
+
+for PKG in "${PKGS[@]}"; do
+	echo "INSTALLING: ${PKG}"
+	sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo
